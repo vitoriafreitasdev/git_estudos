c7c28224c874e8abf50551c9d89a7a108b7a0636
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-01-22 13:59:42 -0300

    Main igual a division

--- python_file.py
+++ python_file.py
@@ -2,16 +2,18 @@
 n1 = int(input("Coloque o primeiro numero: "))
 n2 = int(input("Coloque o segundo numero: "))
 
+add = n1 + n2 
+
 add2 = 20 + 30
 add3 = 230 + 20
-
 
 div = 40 / 2
 div2 = 50 / 2
 
 division = n1 / n2
 
-print(f"Outras adição\n20 + 30 = {add2}\n230 + 20 = {add3}")
-print(f"Outras divisões\n40 / 2 = {div}\n50 / 2 = {div2}")
+print(f"Resultado de {n1} + {n2} = {add}")
+print(f"Resultado de {n1} / {n2} = {division}")
 
-print("Divisão: ", division)
+print(f"Outras adição\n20 + 30 = {add2}\n230 + 20 = {add3}")
+print(f"Outras divisões\n40 / 2 = {div}\n50 / 3 = {div2}")
