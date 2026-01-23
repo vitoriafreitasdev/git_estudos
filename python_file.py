ebfc44b4ddd7bc3c298933d00cf7b55fc4171598
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-01-23 12:26:11 -0300

    Conta mais complexa adicionada

--- python_file.py
+++ python_file.py
@@ -20,9 +20,13 @@ div2 = 80 / 4
 
 division = n1 / n2
 
+conta = (((n1 * 2) * 10) / 2) ** 2  
+
 print(f"Resultado de {n1} + {n2} = {add}")
 print(f"Resultado de {n1} - {n2} = {sub}")
 print(f"Resultado de {n1} / {n2} = {division}")
 
 print(f"Outras adições\n20 + 30 = {add2}\n230 + 20 = {add3}")
 print(f"Outras divisões\n40 / 2 = {div}\n50 / 3 = {div2}")
+
+print(f"(((n1 x 2) x 10) / 2)^2 = ", conta)
